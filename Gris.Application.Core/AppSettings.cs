4fbb977e438c1507fe42c609755e4e283811915c
Ahmed Sayed <ahmed.sayed.abdelmoniem@gmail.com>
2017-04-03 22:57:30 +0200

    add AppSettings helper class

---
+++ Gris.Application.Core/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Gris.Application.Core
+{
+    public static class AppSettings
+    {
+        public static int PageSize
+        {
+            get { return int.Parse(ConfigurationManager.AppSettings["PageSize"]); }
+        }
+    }
+}
